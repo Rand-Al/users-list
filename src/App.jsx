34c90670044db6600d36ad5e28e2b3accad04ea9
Rand-Al <ezusod@gmail.com>
2023-04-03 23:23:40 +0300

    Add table

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App ">
+      <table className="resp-tab">
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Posts</th>
+            <th>Albums</th>
+            <th>Todos</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>User 1</td>
+            <td>Posts Count</td>
+            <td>Album Count</td>
+            <td>Todos Count</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
